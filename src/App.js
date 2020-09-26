f054b62f1a835a498aba6cb9ecf148c007cbb3ff
Adam Saenzpardo <saenzpardoa@gmail.com>
2020-09-26 14:45:45 -0500

    amplified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 class="header">Adam's playground</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
